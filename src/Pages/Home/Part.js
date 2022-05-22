eeeba3151061d37e02aef67d6c7da49249315ee1
Mohuya748 <khandakermohuya@gmail.com>
2022-05-22 17:12:43 +0600

    favicon added

--- src/Pages/Home/Part.js
+++ src/Pages/Home/Part.js
@@ -12,7 +12,7 @@ const Part = ({ part }) => {
         <div class="card card-compact w-96 bg-base-100 shadow-xl">
             <figure><img src={img} alt="image" /></figure>
             <div class="card-body">
-                <h2 class="card-title">{name}</h2>
+                <h2 class="card-title text-cyan-500">{name}</h2>
                 <p><span className='font-bold'>Price :</span> {price}</p>
                 <p><span className='font-bold'>Short Description :</span> {description}</p>
                 <p><span className='font-bold'>Available Quantity :</span> {available_Quantity}</p>
